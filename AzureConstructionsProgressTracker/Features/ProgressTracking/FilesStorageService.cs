3cd9f70bd5b62681b1397dbdd628947c06f22de0
rwalczak <r@fp.pl>
2015-11-17 10:44:17 +0100

    task2

--- AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
+++ AzureConstructionsProgressTracker/Features/ProgressTracking/FilesStorageService.cs
@@ -1,5 +1,6 @@
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web;
 
@@ -18,7 +19,24 @@ namespace AzureConstructionsProgressTracker.Features.ProgressTracking
             // - create container
             // - upload blob
             // - return URL
-            throw new NotImplementedException();
+
+            string BlogconnectionString = "DefaultEndpointsProtocol=https;AccountName=masazurestorage2;AccountKey=CCAWxGDj7z8ItWqpZz9uOTkmi/EwsjwGuFmRKSXY+x6aSCMzx7xiAEMnVgenm+XwpvRcU4gAmf+kHLtBMPP2aQ==";
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(BlogconnectionString);
+            CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
+
+            // Retrieve a reference to a container.
+            CloudBlobContainer container = blobClient.GetContainerReference("mycontainer");
+
+            // Create the container if it doesn't already exist.
+            container.CreateIfNotExists();
+
+            container.SetPermissions(new BlobContainerPermissions{     PublicAccess =    BlobContainerPublicAccessType.Blob    });
+
+            // Retrieve reference to a blob named "myblob".
+            CloudBlockBlob blockBlob = container.GetBlockBlobReference(fileName);
+            await blockBlob.UploadFromStreamAsync(file.InputStream);
+
+            return blockBlob.Uri.AbsoluteUri;
         }
     }
 }
